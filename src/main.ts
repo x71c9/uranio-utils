0cd569ec6bbf34fdabe979f0607ff1fb3c43cde5
Andrea Reni <mail@nbl7.net>
2020-10-18 17:25:50 +0200

    refactor with export namespace

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,27 @@
-import * as log from './log/index';
+/*
+ * Merging all under a namespace
+ */
 
-import * as response from './response/index';
+import * as urn_log from './log/index';
 
-import * as ureturn from './return/index';
+import * as urn_response from './response/index';
 
-import * as util  from './util/index';
+import * as urn_return from './return/index';
 
-export default {
-	log,
-	response,
-	ureturn,
-	util
-};
+import * as urn_util  from './util/index';
+
+export namespace urn_lib {
+	
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	export import log = urn_log;
+	
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	export import response = urn_response;
+	
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	export import ureturn = urn_return;
+	
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	export import util = urn_util;
+	
+}
