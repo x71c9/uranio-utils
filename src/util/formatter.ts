c2d2eec5a2d2938864b63151b2bdc86029e23fb5
x71c9 <108585118+x71c9@users.noreply.github.com>
2020-10-24 10:59:09 +0200

    fixed log_level for debug, === on json_one_line

--- src/util/formatter.ts
+++ src/util/formatter.ts
@@ -4,13 +4,15 @@
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export function json_one_line(obj:any, white_space=' ')
 		:string{
-	if(obj == null || typeof obj != 'object'){
+	if(obj === null || typeof obj !== 'object'){
 		return '';
 	}
 	try{
 		return JSON.stringify(
 			obj,
-			(k,v) => { return v === undefined || k === undefined ? 'undefined' : v; },
+			(k,v) => {
+				return v === undefined || k === undefined ? 'undefined' : v;
+			},
 			white_space
 		);
 	}catch(e){
