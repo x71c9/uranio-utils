ccf93b1ec0272c1db47aba5043df1812b6a5febf
x71c9 <108585118+x71c9@users.noreply.github.com>
2020-11-10 17:12:38 +0100

    json_one_line fixed for Set

--- src/util/formatter.ts
+++ src/util/formatter.ts
@@ -11,6 +11,15 @@ export function json_one_line(obj:any, white_space=' ')
 		return JSON.stringify(
 			obj,
 			(k,v) => {
+				if(v instanceof Set){
+					let set_string = `Set(${v.size})`;
+					set_string += ` { `;
+					const array_set = Array.from(v);
+					const set_elements = array_set.map(el => `'${el}'`).join(', ');
+					set_string += set_elements + ` }`;
+					v = set_string;
+				}
+				v = (v instanceof Set) ? Array.from(v).toString() : v;
 				return v === undefined || k === undefined ? 'undefined' : v;
 			},
 			white_space
