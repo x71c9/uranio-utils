1c8f9024df831e1a4c16645be5af722a5ef6133b
Andrea Reni <mail@nbl7.net>
2021-07-24 17:58:46 +0200

    changed json function name.

--- src/util/json.ts
+++ src/util/json.ts
@@ -2,7 +2,7 @@
  * Util module for JSON
  *
  */
-export function safe_json_stringify(obj:unknown):string {
+export function safe_stringify(obj:unknown):string {
 	try{
 		return JSON.stringify(obj);
 	}catch(ex){
