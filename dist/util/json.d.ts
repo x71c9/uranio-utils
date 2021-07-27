089180d226460c717f2d777958d09608a18b6a51
Andrea Reni <mail@nbl7.net>
2021-07-27 19:11:15 +0200

    compiled json clean parse.

--- dist/util/json.d.ts
+++ dist/util/json.d.ts
@@ -2,5 +2,6 @@
  * Util module for JSON
  *
  */
+export declare function clean_parse(json: string): any;
 export declare function safe_stringify(obj: unknown): string;
 export declare function safe_stringify_oneline(obj: any, white_space?: string): string;
