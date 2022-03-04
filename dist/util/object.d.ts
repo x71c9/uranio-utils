ccfd7690ef5d678ee41a33203cabe4bc06df18f0
x71c9 <108585118+x71c9@users.noreply.github.com>
2022-03-04 09:49:31 +0100

    compiled.

--- dist/util/object.d.ts
+++ dist/util/object.d.ts
@@ -1,6 +1,3 @@
-/**
- * Util module for Objects
- *
- */
 export declare function has_key<O>(obj: O, key: keyof any): key is keyof O;
 export declare function serialize(obj: unknown, prefix?: string): string;
+export declare function deep_clone<T>(obj: T): T;
--- dist/util/object.js
+++ dist/util/object.js
@@ -1,10 +1,30 @@
 "use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.serialize = exports.has_key = void 0;
+exports.deep_clone = exports.serialize = exports.has_key = void 0;
 /**
  * Util module for Objects
  *
  */
+const json = __importStar(require("./json"));
 /*
  * Returns a list of keys to use in loops so Typescript do not complain about keys
  *
@@ -51,4 +71,11 @@ function serialize(obj, prefix = '') {
     return str.join("&");
 }
 exports.serialize = serialize;
+function deep_clone(obj) {
+    if (!obj || typeof obj !== 'object') {
+        return obj;
+    }
+    return json.clean_parse(json.safe_stringify(obj));
+}
+exports.deep_clone = deep_clone;
 //# sourceMappingURL=object.js.map
